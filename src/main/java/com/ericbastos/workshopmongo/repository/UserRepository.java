a05e087716814fb5f96417facda435f8854473ac
Eric1557 <ericbbarbosa2019@gmail.com>
2026-01-29 11:50:02 -0300

    Operação de instanciação da base de dados

--- src/main/java/com/ericbastos/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/ericbastos/workshopmongo/repository/UserRepository.java
@@ -1,10 +1,9 @@
 package com.ericbastos.workshopmongo.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
 import com.ericbastos.workshopmongo.domain.User;
-@Repository
+
 public interface UserRepository extends MongoRepository<User, String>{
 
 }
