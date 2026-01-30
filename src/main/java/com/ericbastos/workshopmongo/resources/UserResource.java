a2e6835d9d66539a36786fda0ca91480054ac65b
Eric1557 <ericbbarbosa2019@gmail.com>
2026-01-30 12:55:13 -0300

    Usando padrão DTO para retornar usuários

--- src/main/java/com/ericbastos/workshopmongo/resources/UserResource.java
+++ src/main/java/com/ericbastos/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.ericbastos.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ericbastos.workshopmongo.domain.User;
+import com.ericbastos.workshopmongo.dto.UserDTO;
 import com.ericbastos.workshopmongo.services.UserServices;
 
 @RestController
@@ -19,9 +21,10 @@ public class UserResource {
 	private UserServices services;
 	
  @RequestMapping(method=RequestMethod.GET)	
-  public ResponseEntity<List<User>> findAll(){
+  public ResponseEntity<List<UserDTO>> findAll(){
       List<User> list = services.FindAll();
-      return ResponseEntity.ok().body(list);
+      List<UserDTO> listdto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+      return ResponseEntity.ok().body(listdto);
   }
 
 }
