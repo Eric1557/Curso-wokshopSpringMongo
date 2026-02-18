6f16e5aa968835f90557a0169b2ffec7b771305b
Eric1557 <ericbbarbosa2019@gmail.com>
2026-02-18 18:10:31 -0300

    Obtendo um post por id

---
+++ src/main/java/com/ericbastos/workshopmongo/resources/PostResource.java
@@ -0,0 +1,71 @@
+package com.ericbastos.workshopmongo.resources;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.ericbastos.workshopmongo.domain.Post;
+import com.ericbastos.workshopmongo.domain.User;
+import com.ericbastos.workshopmongo.dto.UserDTO;
+import com.ericbastos.workshopmongo.services.UserServices;
+
+@RestController
+@RequestMapping("/users")
+public class PostResource {
+	
+	@Autowired
+	private UserServices services;
+	
+ @RequestMapping(method=RequestMethod.GET)	
+  public ResponseEntity<List<UserDTO>> findAll(){
+      List<User> list = services.FindAll();
+      List<UserDTO> listdto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+      return ResponseEntity.ok().body(listdto);
+  }
+ 
+ @RequestMapping(value="/{id}",method=RequestMethod.GET)	
+ public ResponseEntity<UserDTO> findByld(@PathVariable String id){
+	 User obj = services.findByld(id); 
+     return ResponseEntity.ok().body(new UserDTO(obj));
+    
+   }
+ 
+ @RequestMapping(method=RequestMethod.POST)
+ public ResponseEntity<Void> Insert(@RequestBody UserDTO objDto){
+	User obj = services.FromDTO(objDto);
+	obj = services.Insert(obj);
+	URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+	return ResponseEntity.created(uri).build();
+ }
+ 
+ @RequestMapping(value= "/{id}",method=RequestMethod.DELETE)
+ public ResponseEntity<Void> delete(@PathVariable String id){
+	 services.delete(id);
+	 return ResponseEntity.noContent().build();
+ }
+ 
+ @RequestMapping(value="/{id}",method=RequestMethod.PUT)
+ public ResponseEntity<Void> update(@PathVariable String id,@RequestBody UserDTO objDTO){
+	 User obj = services.FromDTO(objDTO);
+	 obj.setId(id);
+	 obj = services.update(obj);
+	 URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj).toUri();
+	  return ResponseEntity.created(uri).build(); 
+ }
+ 
+ @RequestMapping(value="/{id}/posts",method=RequestMethod.GET)
+ public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+	 User obj = services.findByld(id);
+	 return ResponseEntity.ok().body(obj.getPosts());
+ }
+ 
+}
---
+++ src/main/java/com/ericbastos/workshopmongo/services/PostServices.java
@@ -0,0 +1,23 @@
+package com.ericbastos.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ericbastos.workshopmongo.domain.Post;
+import com.ericbastos.workshopmongo.repository.PostRepository;
+import com.ericbastos.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostServices {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post FindByld(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+	}
+    
+}
